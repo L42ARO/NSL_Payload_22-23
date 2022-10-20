476f31bc653b5db98a772dfe7cd0378ad77e9628
rio-okada <rokada@usf.edu>
2022-10-20 17:48:47 -0400

    String encoded into utf-8 serial data.

--- TestingDev/SerialCommunicationTest/RaspberryPiPython.py
+++ TestingDev/SerialCommunicationTest/RaspberryPiPython.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     ser.reset_input_buffer()
 
     while True:
-        ser.write("Hello from Raspberry Pi!\n")
+        ser.write("Hello from Raspberry Pi!\n".encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
         time.sleep(1)
