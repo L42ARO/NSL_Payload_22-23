2a35b9de548509021be89674917169b39f73955e
alvaro512 <alvaro512@usf.edu>
2022-11-11 22:48:13 -0500

    Found ideal delay values for stepper control

--- pico_Dev/PIO_PicoDev/src/main.cpp
+++ pico_Dev/PIO_PicoDev/src/main.cpp
@@ -2,30 +2,33 @@
 // defines pins numbers
 const int stepPin = 6; 
 const int dirPin = 5; 
- 
+const int microDelay = 1000;
+const int betweenDelay = 250;
 void setup() {
   // Sets the two pins as Outputs
   pinMode(stepPin,OUTPUT); 
   pinMode(dirPin,OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 void loop() {
+  digitalWrite(LED_BUILTIN, HIGH);
   digitalWrite(dirPin,HIGH); // Enables the motor to move in a particular direction
   // Makes 200 pulses for making one full cycle rotation
   for(int x = 0; x < 200; x++) {
     digitalWrite(stepPin,HIGH); 
-    delayMicroseconds(500); 
+    delayMicroseconds(microDelay); 
     digitalWrite(stepPin,LOW); 
-    delayMicroseconds(500); 
+    delayMicroseconds(betweenDelay); 
   }
   delay(1000); // One second delay
-  
+  digitalWrite(LED_BUILTIN,LOW);
   digitalWrite(dirPin,LOW); //Changes the rotations direction
   // Makes 400 pulses for making two full cycle rotation
   for(int x = 0; x < 400; x++) {
     digitalWrite(stepPin,HIGH);
-    delayMicroseconds(500);
+    delayMicroseconds(microDelay);
     digitalWrite(stepPin,LOW);
-    delayMicroseconds(500);
+    delayMicroseconds(betweenDelay);
   }
   delay(1000);
 }
