15d512d7599f5db118634b4ce7f9f596cdf5a700
Fuuxd <realsociologia@gmail.com>
2022-11-18 20:20:20 -0500

    Start of command velocity

--- ZERO-DEV/mods/happy_landing.py
+++ ZERO-DEV/mods/happy_landing.py
@@ -1,13 +1,22 @@
 import adafruit_bno055
 import board
+import math
 import time
+
+
 i2c = board.I2C()  # uses board.SCL and board.SDA
 sensor = adafruit_bno055.BNO055_I2C(i2c)
 def checkForLanding():
-    while(True):
-        velocity = sensor.acceleration
-        print(f"Velocity: {velocity}", end="\r")
+    flag = 1
+    while(flag == 1):
+        velocity = sensor.linear_acceleration
+        magnitude = math.sqrt( (velocity[0] ** 2) + (velocity[1] ** 2) + (velocity[2] ** 2) )
+        print(f"Velocity: {velocity} magnitude: {magnitude}", end="\r")
+        if (magnitude >= 69):
+            flag = 0
         time.sleep(1)
+    time.sleep(90)
+
     return
 
 if __name__=="__main__":
