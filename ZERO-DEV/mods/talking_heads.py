19e623a5e6d6252355cdafed74a8a8051c010f2c
Fuuxd <realsociologia@gmail.com>
2022-11-19 01:24:48 -0500

    serialcommunication_testing_ttyACM0

--- ZERO-DEV/mods/talking_heads.py
+++ ZERO-DEV/mods/talking_heads.py
@@ -4,12 +4,14 @@ import serial
 import time
     
 def talk():
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.reset_input_buffer()
 
     while True:
-        ser.write("Hello from Raspberry Pi!\n".encode('utf-8'))
+        ser.write('s'.encode('utf-8'))
         line = ser.readline().decode('utf-8').rstrip()
+        if (line == '1'):
+            print("Success.\n")
         print(line)
         time.sleep(1)
 if __name__ == "__main__":
