15d7d73518a2ff0689131eabbe5f265e21410fed
rio-okada <rokada@usf.edu>
2023-01-19 18:12:22 -0500

    microstepper code 360 degrees

--- TestingDev/Microstepper/microstepperTest.ino
+++ TestingDev/Microstepper/microstepperTest.ino
@@ -1,33 +1,40 @@
+float starttime;
+
 void setup() {
-pinMode(1 , OUTPUT);
-pinMode(2, OUTPUT);
-pinMode(3 , OUTPUT);
-pinMode(4, OUTPUT);
-
-pinMode(5 , OUTPUT);
-pinMode(6, OUTPUT);
-pinMode(7 , OUTPUT);
-pinMode(8, OUTPUT);
-
-}
-void loop() {
-
-
-  float floatTime = float(millis());
-  float A = (sin((floatTime * 0.03) )) * 255;
-  float B = (sin((floatTime * 0.03) + 1.5708)) * 255;
-  analogWrite(1, max(0, A));
-  analogWrite(2, max(0,-A));
-  analogWrite(3, max(0, B));
-  analogWrite(4, max(0,-B));
-
-  analogWrite(5, max(0, A));
-  analogWrite(6, max(0,-A));
-  analogWrite(7, max(0, B));
-  analogWrite(8, max(0,-B));
+  starttime = millis();
   
+  pinMode(3 , OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9 , OUTPUT);
+  pinMode(10, OUTPUT);
 
-delay(1000);
+  Serial.begin(9600);
+}
+
+void loop() {
+  static int count{0};
+  Serial.println(++count);
+
+  if (count >= 20){
+    return;
+  }
+  
+  float floatTime = float(millis()) - starttime;
+  float A = (sin((floatTime * 0.3) )) * 255;
+  float B = (sin((floatTime * 0.3) + 1.5708)) * 255;
+  
+  analogWrite(3, max(0, A));
+  analogWrite(11, max(0,-A));
+  analogWrite(9, max(0, B));
+  analogWrite(10, max(0,-B));
+
+  analogWrite(A0, max(0, A));
+  analogWrite(A1, max(0,-A));
+  analogWrite(A2, max(0, B));
+  analogWrite(A3, max(0,-B));
+
+  
+  delay(300);
 /*for(int i=0; i<20; i++){
   float floatTime = float(millis());
   float A = (sin((floatTime * 0.03) )) * 255;
@@ -41,10 +48,10 @@ delay(1000);
   analogWrite(6, max(0,A));
   analogWrite(7, max(0,-B));
   analogWrite(8, max(0,B));
-  /*
+  
 }
 
 delay(1);
-
+*/
 
 }
