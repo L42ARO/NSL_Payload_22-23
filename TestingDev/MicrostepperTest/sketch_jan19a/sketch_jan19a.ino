f4cce03d03b071f38a246704356988e8cd85c13b
rio-okada <rokada@usf.edu>
2023-01-19 20:02:03 -0500

    working microcontroller code

---
+++ TestingDev/MicrostepperTest/sketch_jan19a/sketch_jan19a.ino
@@ -0,0 +1,92 @@
+#include <Arduino.h>
+
+const int full_rot_count{ 20 };
+const int delay_btwn_loop{ 300 };
+const double phase_shift_B{ 1.5708 };   // pi/2
+const double delta_time_factor{ 0.3 };
+
+enum class Micro1pins {
+    A_plus = 3, A_minus = 11, B_plus = 9, B_minus = 11
+};
+
+enum class Micro2pins {
+    A_plus = A0, A_minus = A1, B_plus = A2, B_minus = A3
+};
+
+void moveMicrostepper1(double angle, bool clockwise);
+void moveMicrostepper2(double angle, bool clockwise);
+
+void setup()
+{
+  pinMode(3 , OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9 , OUTPUT);
+  pinMode(10, OUTPUT);
+
+  Serial.begin(9600);
+}
+
+int count = 0;
+void loop(){
+  if(count <= 0) {
+    Serial.print("Calling: ");
+    Serial.println(count);
+    moveMicrostepper2(PI/2, false);
+  }
+  count = 2;
+}
+
+
+void moveMicrostepper1(double angle, bool clockwise)
+{
+  int steps{ round(angle / (2*PI) * full_rot_count) };
+  float starttime{ millis() };
+
+  for (int i{0}; i < steps; ++i) {
+    float floatTime = float(millis()) - starttime;
+    float A = (sin((floatTime * delta_time_factor) )) * 255;
+    float B = (sin((floatTime * delta_time_factor) + phase_shift_B)) * 255;
+  
+    if (!clockwise)  {
+      analogWrite((int)Micro1pins::A_plus, max(0, A));
+      analogWrite((int)Micro1pins::A_minus, max(0,-A));
+      analogWrite((int)Micro1pins::B_plus, max(0, B));
+      analogWrite((int)Micro1pins::B_minus, max(0,-B));
+    }
+    else {
+      analogWrite((int)Micro1pins::A_plus, max(0, -B));
+      analogWrite((int)Micro1pins::A_minus, max(0, B));
+      analogWrite((int)Micro1pins::B_plus, max(0, -A));
+      analogWrite((int)Micro1pins::B_minus, max(0, A));
+    }
+    delay(delay_btwn_loop);
+  }
+}
+
+void moveMicrostepper2(double angle, bool clockwise)
+{
+  int steps = round(angle / (2*PI) * full_rot_count);
+  float starttime = millis();
+
+  for (int i=0; i < steps; ++i) {
+    Serial.println(i);
+    float floatTime = float(millis()) - starttime;
+    float A = (sin((floatTime * delta_time_factor) )) * 255;
+    float B = (sin((floatTime * delta_time_factor) + phase_shift_B)) * 255;
+  
+    if (!clockwise) {
+      analogWrite((int)Micro2pins::A_plus, max(0, A));
+      analogWrite((int)Micro2pins::A_minus, max(0,-A));
+      analogWrite((int)Micro2pins::B_plus, max(0, B));
+      analogWrite((int)Micro2pins::B_minus, max(0,-B));
+    }
+    else {
+      analogWrite((int)Micro2pins::A_plus, max(0, -B));
+      analogWrite((int)Micro2pins::A_minus, max(0, B));
+      analogWrite((int)Micro2pins::B_plus, max(0, -A));
+      analogWrite((int)Micro2pins::B_minus, max(0, A));
+    }
+
+    delay(delay_btwn_loop);
+  }
+}
