34f57ee24c452065fb0860ef678cfa9e6c62dbfb
alvaro512 <alvaro512@usf.edu>
2023-01-24 17:55:43 -0500

    Bullseye+talkingheads+extra code: - Added some functions in bullseye that will be needed later - Modified talking heads to use I2C protocol - Added the code that would make the piezzo emit the mario theme song

--- ZERO-DEV/mods/bullseye.py
+++ ZERO-DEV/mods/bullseye.py
@@ -1,3 +1,4 @@
+import cv2
 from picamera import PiCamera
 from time import sleep
 from datetime import datetime
@@ -27,6 +28,25 @@ def SeriesOfPics():
     if run == False: return
     for i in range(3):
         TakePhoto(i)
+
+def add_timestamp(img):
+    # Get current time
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    # Add timestamp to the upper right corner of the image
+    cv2.putText(img, timestamp, (img.shape[1]-150,30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+    # Return the image
+    return img
+
+def rotate_image(img, degree):
+    # Get image size
+    rows,cols = img.shape[:2]
+    # Define rotation matrix
+    M = cv2.getRotationMatrix2D((cols/2,rows/2),degree,1)
+    # Perform rotation
+    img_rotated = cv2.warpAffine(img,M,(cols,rows))
+    # Return rotated image
+    return img_rotated
+
 
 if __name__=="__main__":
     for i in range(3):
--- ZERO-DEV/mods/talking_heads.py
+++ ZERO-DEV/mods/talking_heads.py
@@ -1,33 +1,57 @@
 
 #!/usr/bin/env python3
-import serial
+#import serial
+import smbus
 import time
-    
-def talk(cmd):
+
+bus = smbus.SMBus(1)
+address = 8
+
+#Returns 1 if successful, 0 if timeout
+def talk(command_number, value, timeout):
     try:
-        flag = True
-        print(f"Sending: {cmd}")
-        while(flag):
-            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
-            ser.reset_input_buffer()
-            ser.write((cmd+'\n').encode('utf-8'))
-            line = ser.readline().decode('utf-8').rstrip()
-            if (line == cmd):
-                flag = False
-        print("Received: ", line)
-
-        flag = True
-        while(flag):
-            line = ser.readline().decode('utf-8').rstrip()
-            if (line == 'High'):
-                flag = False
-
-        ser.write('0\n'.encode('utf-8'))
-    
-        time.sleep(1)
-        pass
+        command = str(command_number) + "_" + str(value)
+        bus.write_i2c_block_data(address, 0, list(command.encode()))
+        start_time = time.time()
+        while True:
+            data = bus.read_i2c_block_data(address, 0)
+            data = ''.join(map(chr, data))
+            if data == "ready":
+                print("Success")
+                return 1
+            if time.time() - start_time > timeout:
+                print("Error: Timeout")
+                return 0
+            time.sleep(0.1)
     except Exception as e:
         print(f'Error talking to Pico: {e}')
+        return 0
+
+# def talk(cmd):
+#     try:
+#         flag = True
+#         print(f"Sending: {cmd}")
+#         while(flag):
+#             ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+#             ser.reset_input_buffer()
+#             ser.write((cmd+'\n').encode('utf-8'))
+#             line = ser.readline().decode('utf-8').rstrip()
+#             if (line == cmd):
+#                 flag = False
+#         print("Received: ", line)
+
+#         flag = True
+#         while(flag):
+#             line = ser.readline().decode('utf-8').rstrip()
+#             if (line == 'High'):
+#                 flag = False
+
+#         ser.write('0\n'.encode('utf-8'))
+    
+#         time.sleep(1)
+#         pass
+#     except Exception as e:
+#         print(f'Error talking to Pico: {e}')
 
 
 if __name__ == "__main__":
