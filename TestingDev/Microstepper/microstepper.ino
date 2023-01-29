975f6475988fc80ce892415e0280f10b8e5489dc
Fuuxd <realsociologia@gmail.com>
2023-01-29 03:49:27 -0500

    merge?

--- TestingDev/Microstepper/microstepper.ino
+++ TestingDev/Microstepper/microstepper.ino
@@ -1,92 +1,131 @@
 #include <Arduino.h>
 
-const int full_rot_count{ 20 };
-const int delay_btwn_loop{ 300 };
-const double phase_shift_B{ 1.5708 };   // pi/2
-const double delta_time_factor{ 0.3 };
+/************ Microstepper Class ****************/
+class Microstepper {
+  public:
+    struct PinList{
+      int A_plus;
+      int A_minus;
+      int B_plus;
+      int B_minus;
+    };
+    Microstepper() = delete;
+    Microstepper(int full_rot_count, int delay_btwn_loop, PinList pins)
+      : full_rot_count(full_rot_count), delay_btwn_loop(delay_btwn_loop), pins(pins)
+    {
+      // Calibration to set current_idx to some value.  PI/3 is arbitary.  The microstpeper sometimes overshoot
+      // in its first run because current_idx is unknown.
+      rotate(PI/3, true);
+      delay(1000);
+    }
+    void rotate(double angle, bool clockwise);
 
-enum class Micro1pins {
-    A_plus = 3, A_minus = 11, B_plus = 9, B_minus = 10
+  private:
+    const int full_rot_count = 42;
+    const int delay_btwn_loop = 50;
+    const PinList pins;
+    static const double phase_shift_B;   // pi/2
+    static const double delta_time_factor;
+    static const int rotation_bitmap[8][4];
+
+    // Keeps track of where in the rotation bitmap it is at.
+    int current_idx = -1;
 };
 
-enum class Micro2pins {
-    A_plus = A0, A_minus = A1, B_plus = A2, B_minus = A3
+// Initializing static member variables in Microstepper class.
+const double Microstepper::phase_shift_B = 1.5708;
+const double Microstepper::delta_time_factor = 0.3;
+const int Microstepper::rotation_bitmap[8][4] = {
+      {HIGH, LOW, LOW, LOW},
+      {HIGH, LOW, HIGH, LOW},
+      {LOW, LOW, HIGH, LOW},
+      {LOW, HIGH, HIGH, LOW},
+      {LOW, HIGH, LOW, LOW},
+      {LOW, HIGH, LOW, HIGH},
+      {LOW, LOW, LOW, HIGH},
+      {HIGH, LOW, LOW, HIGH}
 };
 
-void moveMicrostepper1(double angle, bool clockwise);
-void moveMicrostepper2(double angle, bool clockwise);
+void Microstepper::rotate(double angle, bool clockwise)
+{
+  int steps{ round(angle / (2*PI) * full_rot_count) };
+  if (current_idx == -1) 
+    current_idx = clockwise ? 0 : 7;
+
+  for (int i{0}; i < steps; ++i) {
+    digitalWrite(pins.A_plus, rotation_bitmap[current_idx][0]);
+    digitalWrite(pins.A_minus, rotation_bitmap[current_idx][1]);
+    digitalWrite(pins.B_plus, rotation_bitmap[current_idx][2]);
+    digitalWrite(pins.B_minus, rotation_bitmap[current_idx][3]);
+    
+    // Increment if clockwise is true, decrement if false
+    clockwise ? ++current_idx : --current_idx;
+
+    if (current_idx < 0) {
+      current_idx = 7;
+    }
+    else if (current_idx > 7) {
+      current_idx = 0;
+    }
+  
+    delay(delay_btwn_loop);
+  }
+
+  // Undo last increment because rotation direction might change
+  clockwise ? --current_idx : ++current_idx;
+}
+
+/*************** Microstepper Class End *********************/
 
 void setup()
 {
-  pinMode((int)Micro1pins::A_plus, OUTPUT);
-  pinMode((int)Micro1pins::A_minus, OUTPUT);
-  pinMode((int)Micro1pins::B_plus, OUTPUT);
-  pinMode((int)Micro1pins::B_minus, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 
   Serial.begin(9600);
 }
 
 int count = 0;
-void loop(){
+// steps = 42.  delay = 50 or 500
+
+void loop()
+{
+  Microstepper stepper1(42, 50, {3, 11, 9, 10});
+
   if(count <= 0) {
-    Serial.print("Calling: ");
+    Serial.print("\nStarting: ");
     Serial.println(count);
-    moveMicrostepper2(PI/2, false);
+
+    // stepper1.rotate(PI/2, true);
+    // delay(2000);
+    // stepper1.rotate(PI/2, true);
+    // delay(2000);
+    // stepper1.rotate(PI/2, true);
+    // delay(2000);
+    // stepper1.rotate(PI/2, true);
+    // delay(2000);
+
+    // stepper1.rotate(2*PI, false);
+    // delay(2000);
+    // stepper1.rotate(2*PI, true);
+    // delay(2000);
+
+    stepper1.rotate(PI/3, true);
+    delay(2000);
+    stepper1.rotate(PI/3, true);
+    delay(2000);
+    stepper1.rotate(PI/3, true);
+    delay(2000);
+    stepper1.rotate(PI/3, false);
+    delay(2000);
+    stepper1.rotate(PI/3, false);
+    delay(2000);
+    stepper1.rotate(PI/3, false);
+    delay(2000);
   }
-  count = 2;
-}
 
-
-void moveMicrostepper1(double angle, bool clockwise)
-{
-  int steps{ round(angle / (2*PI) * full_rot_count) };
-  float starttime{ millis() };
-
-  for (int i{0}; i < steps; ++i) {
-    float floatTime = float(millis()) - starttime;
-    float A = (sin((floatTime * delta_time_factor) )) * 255;
-    float B = (sin((floatTime * delta_time_factor) + phase_shift_B)) * 255;
-  
-    if (!clockwise)  {
-      analogWrite((int)Micro1pins::A_plus, max(0, A));
-      analogWrite((int)Micro1pins::A_minus, max(0,-A));
-      analogWrite((int)Micro1pins::B_plus, max(0, B));
-      analogWrite((int)Micro1pins::B_minus, max(0,-B));
-    }
-    else {
-      analogWrite((int)Micro1pins::A_plus, max(0, -B));
-      analogWrite((int)Micro1pins::A_minus, max(0, B));
-      analogWrite((int)Micro1pins::B_plus, max(0, -A));
-      analogWrite((int)Micro1pins::B_minus, max(0, A));
-    }
-    delay(delay_btwn_loop);
-  }
-}
-
-void moveMicrostepper2(double angle, bool clockwise)
-{
-  int steps = round(angle / (2*PI) * full_rot_count);
-  float starttime = millis();
-
-  for (int i=0; i < steps; ++i) {
-    Serial.println(i);
-    float floatTime = float(millis()) - starttime;
-    float A = (sin((floatTime * delta_time_factor) )) * 255;
-    float B = (sin((floatTime * delta_time_factor) + phase_shift_B)) * 255;
-  
-    if (!clockwise) {
-      analogWrite((int)Micro2pins::A_plus, max(0, A));
-      analogWrite((int)Micro2pins::A_minus, max(0,-A));
-      analogWrite((int)Micro2pins::B_plus, max(0, B));
-      analogWrite((int)Micro2pins::B_minus, max(0,-B));
-    }
-    else {
-      analogWrite((int)Micro2pins::A_plus, max(0, -B));
-      analogWrite((int)Micro2pins::A_minus, max(0, B));
-      analogWrite((int)Micro2pins::B_plus, max(0, -A));
-      analogWrite((int)Micro2pins::B_minus, max(0, A));
-    }
-
-    delay(delay_btwn_loop);
-  }
+  count = 0;
+  delay(2000);
 }
