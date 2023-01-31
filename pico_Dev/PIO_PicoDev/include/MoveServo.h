fe8db96f38a07f4b75f56354f929df2392575349
rio-okada <rokada@usf.edu>
2023-01-30 20:06:06 -0500

    MainServo class defined

--- pico_Dev/PIO_PicoDev/include/MoveServo.h
+++ pico_Dev/PIO_PicoDev/include/MoveServo.h
@@ -4,16 +4,21 @@
 #include <servo.h> 
 //DONT KNOW IF I NEED TO REINCLUDE ARDUINO.H HERE
 
-void MoveServo(int startAngle, int endAngle) 
-{
-  const int i = startAngle > endAngle ? -1 : 1;
+class MainServo {
+  public:
+    void rotate(int startAngle, int endAngle) 
+    {
+      const int i = startAngle > endAngle ? -1 : 1;
 
-  for (int pos{ startAngle }; pos != endAngle; pos+=i) {
-    myservo.write(pos);
-    delay(15);
-  }
-  myservo.write(endAngle);
-}
-//saving space by excluding implementation 
+      for (int pos{ startAngle }; pos != endAngle; pos+=i) {
+        myservo.write(pos);
+        delay(15);
+      }
+      myservo.write(endAngle);
+    }
+
+  private:
+    Servo myservo;
+};
 
 #endif
