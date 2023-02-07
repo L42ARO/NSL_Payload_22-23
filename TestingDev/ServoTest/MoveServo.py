74a0dfa1e4d82cd17370f754591ff5edf7f85750
Fuuxd <realsociologia@gmail.com>
2023-02-07 18:34:53 -0500

    moveservo

--- TestingDev/ServoTest/MoveServo.py
+++ TestingDev/ServoTest/MoveServo.py
@@ -4,6 +4,7 @@ import board
 import RPi.GPIO as io
 import time
 
+'''
 myservo = Servo(18)
 
 myservo.write(180)
@@ -13,27 +14,28 @@ time.sleep(3)
 myservo.stop()
 
 '''
-class MainServo:
-    def rotate(self, startAngle, endAngle):
-        i = -1 if startAngle > endAngle else 1
 
-        for pos in range(startAngle, endAngle, i):
-            piservo.write(pos)
-            time.sleep(15)
-        piservo.write(endAngle)
+def rotate(servo_, startAngle, endAngle):
+    i = -1 if startAngle > endAngle else 1
+
+    for pos in range(startAngle, endAngle, i):
+        servo_.write(pos)
+        time.sleep(15)
+    piservo.write(endAngle)
     
-    def begin(self):
-        myservo = piservo(21)
+def begin(pin):
+    myservo = piservo(pin)
+    return myservo
 
 if __name__ == "__main__":
     pin = 21
     startAngle = 0
     endAngle = 180
     
-    servo = MainServo()
-    servo.begin(21)
-    servo.rotate(startAngle, endAngle)
+    rotate(begin(21), 0, 180)
 
+
+'''
 DIFFERENT PROGRAM
 
 myservo = Servo(12)
