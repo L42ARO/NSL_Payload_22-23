4457631a6e186815cca785490a730a239b9f7099
Fuuxd <realsociologia@gmail.com>
2023-02-06 20:34:04 -0500

    moveservo

---
+++ ZERO-DEV/mods/MoveServo.py
@@ -0,0 +1,14 @@
+import piServoCtl
+
+class MainServo:
+    def rotate(self, startAngle, endAngle):
+        i = -1 if startAngle > endAngle else 1
+
+        for pos in range(startAngle, endAngle, i):
+            piServoCtl.write(pos)
+            piServoCtl.delay(15)
+        piServoCtl.write(endAngle)
+    
+    def begin(self):
+        piServoCtl.pinMode(pin, piServoCtl.OUTPUT)
+        piServoCtl.attach(pin)
