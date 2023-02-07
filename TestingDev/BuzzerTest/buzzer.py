a0b469cb7020bd2e860eaba77930db7f6703bc4f
rio-okada <rokada@usf.edu>
2023-02-06 19:23:39 -0500

    buzzer test for pi

---
+++ TestingDev/BuzzerTest/buzzer.py
@@ -0,0 +1,18 @@
+#!/user/bin/python
+
+import RPi.GPIO as io
+import time
+
+
+buzzPin = 23
+
+def buzzTest():
+    io.setmode(io.BCM)
+    io.setup(buzzPin, io.OUT)
+
+    while (True):
+        io.output(buzzPin, True)
+        time.sleep(2)
+        io.output(buzzPin, False)
+        time.sleep(2)
+
