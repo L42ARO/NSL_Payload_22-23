bb0ec8d71d38d4556de747024869f67e0c9962b4
rio-okada <rokada@usf.edu>
2023-02-13 18:50:48 -0500

    Moved codes from i2c to main

--- pico_Dev/PIO_PicoDev/src/i2c_com.cpp
+++ pico_Dev/PIO_PicoDev/src/i2c_com.cpp
@@ -8,15 +8,6 @@
 #include "mini_steppers.h"
 #include "microstepper.h"
 
-//const int buzzerPin = 14;
-//const int stepPin = 6; 
-//const int dirPin = 5; 
-//Stepper stepper1(stepPin, dirPin); //create stepper object
-//MainServo mainServo(9);
-//Buzzer time_keeper(buzzerPin); //create buzzer object
-//Microstepper micro1(42, 500, {3, 11, 9, 10});
-//Microstepper micro2(42, 50, {4, 6, 7, 8});
-//
 I2C_Comm* I2C_Comm::instance =0;
 
 void I2C_Comm::begin(int address) {
@@ -24,13 +15,6 @@ void I2C_Comm::begin(int address) {
     Wire.begin(_address);                // join i2c bus with address #8
     Wire.onReceive(receiveEvent); // register event
     instance=this;
-
-    // Setting the pins up
-    //micro1.begin();
-    //micro2.begin();
-    //time_keeper.begin();
-    //stepper1.begin();
-    //mainServo.begin();
 }
 
 // function that executes whenever data is received from master
@@ -105,64 +89,4 @@ void I2C_Comm::receiveEvent(int howMany) {
     //instance->processCommand(commandNumber, value);
     //Wire.write("ready");        // sends ready
     //Wire.endTransmission();     // stop transmitting
-}
-
-void I2C_Comm::processCommand(int commandNumber, int value){
-    //switch(commandNumber){
-    //    case 0:
-    //    //WAIT
-    //        Serial.print("Command 0 recieved with value: ");
-    //        Serial.println(value);
-    //        if(value == '1'){
-    //            time_keeper.setUseBuzzer(1);
-    //        }else if(value == '0'){
-    //            time_keeper.setUseBuzzer(0);
-    //        }
-    //        break;
-    //    case 1:
-    //    //RUN SERVO
-    //        Serial.print("Command 1 received with value: ");
-    //        Serial.println(value);
-    //        time_keeper.setUseBuzzer(0);
-    //        mainServo.rotate(0, value);
-    //        break;
-    //    case 2:
-    //    //RUN STEPPER BIG
-    //        int dir = 1; //CHECK ACTUAL DIRECTIONS dont know if 
-    //        Serial.print("Command 2 received with value: ");
-    //        Serial.println(value);
-    //        time_keeper.setUseBuzzer(0);
-    //        if(value <0){
-    //            value = value * (-1); // do abs() if brave
-    //            dir = 0;
-    //        }
-    //        stepper1.rotate(value, dir);
-    //        break;
-    //    case 3:
-    //    //Run radio frequency decoding
-    //        Serial.print("Command 3 received with value: ");
-    //        Serial.println(value);
-    //        time_keeper.setUseBuzzer(0);
-    //        break;
-    //    case 4:
-    //    //Run 360 degrees microstepper
-    //        Serial.print("Command 4 received with value: ");
-    //        Serial.println(value);
-    //        time_keeper.setUseBuzzer(0);
-    //        micro1.rotate(value);
-    //        break;
-    //    
-    //    case 5:
-    //    //Run camera tilting microstepper
-    //        Serial.print("Command 5 received with value: ");
-    //        Serial.println(value);
-    //        time_keeper.setUseBuzzer(0);
-    //        micro2.rotate(value);
-    //        break;
-
-    //    default:
-    //        time_keeper.setUseBuzzer(0);
-    //        Serial.println("Invalid command received");
-    //        break;
-    //}
 }
--- pico_Dev/PIO_PicoDev/src/main.cpp
+++ pico_Dev/PIO_PicoDev/src/main.cpp
@@ -1,13 +1,83 @@
 #include <Arduino.h>
 #include <i2c_com.h>
 
+#include "MoveStepper.h"
+#include "MoveServo.h"
+#include "buzzerNotification.h"
+#include "microstepper.h"
+
 I2C_Comm &i2c = I2C_Comm::getInstance();
+const int stepPin = 6; 
+const int dirPin = 5; 
+Stepper stepper1(stepPin, dirPin); //create stepper object
+MainServo mainServo(9);
+Microstepper micro1(42, 500, {3, 11, 9, 10});
+Microstepper micro2(42, 50, {4, 6, 7, 8});
 
 void setup() {
   Serial.begin(9600);
   i2c.getInstance().begin(8);
+  // Setting the pins up
+  micro1.begin();
+  micro2.begin();
+  stepper1.begin();
+  mainServo.begin();
 }
 
 void loop() {
   delay(100);
+}
+
+void processCommand(int commandNumber, int value){
+    switch(commandNumber){
+       case 0:
+       //WAIT
+           Serial.print("Command 0 recieved with value: ");
+           Serial.println(value);
+           break;
+           
+       case 1:
+       //RUN SERVO
+           Serial.print("Command 1 received with value: ");
+           Serial.println(value);
+           mainServo.rotate(0, value);
+           break;
+
+       case 2:
+       //RUN STEPPER BIG
+           int dir = 1; //CHECK ACTUAL DIRECTIONS dont know if 
+           Serial.print("Command 2 received with value: ");
+           Serial.println(value);
+           
+           if(value <0){
+               value = value * (-1); // do abs() if brave
+               dir = 0;
+           }
+           stepper1.rotate(value, dir);
+           break;
+
+       case 3:
+       //Run radio frequency decoding
+           Serial.print("Command 3 received with value: ");
+           Serial.println(value);
+           break;
+
+       case 4:
+       //Run 360 degrees microstepper
+           Serial.print("Command 4 received with value: ");
+           Serial.println(value);
+           micro1.rotate(value);
+           break;
+       
+       case 5:
+       //Run camera tilting microstepper
+           Serial.print("Command 5 received with value: ");
+           Serial.println(value);
+           micro2.rotate(value);
+           break;
+
+       default:
+           Serial.println("Invalid command received");
+           break;
+    }
 }
