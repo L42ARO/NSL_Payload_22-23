49074caa24197342702fdb48d4cb8dcfb19c8603
rio-okada <rokada@usf.edu>
2023-02-13 00:29:27 -0500

    check for mainstepper after rotation

--- ZERO-DEV/mods/vector_perkins.py
+++ ZERO-DEV/mods/vector_perkins.py
@@ -1,11 +1,59 @@
 import numpy as np
 import math
 import json
+from mr_blue_sky import getAcceleration
+
 class IMU_DATA:
     def __init__(self, gravity, axis):
         self.gravityVector = gravity
         self.refVectors = [axis[0], axis[1]]
+
+imu1_data = imu2_data = holeList = None
+
+def begin(filename):
+    global imu1_data, imu2_data, holeList
+    data = open_json_file(filename)
+    # Initialize holes
+    holeList = data["holes"]
+    # Initilize IMUs
+    imu1_ref = [np.array(data["imu1"]["refVec1"]), np.array(data["imu1"]["refVec2"])]
+    imu2_ref = [np.array(data["imu2"]["refVec1"]), np.array(data["imu2"]["refVec2"])]
+    accel = getAcceleration()
+    imu1_data = IMU_DATA(np.array(accel[0]),imu1_ref)
+    imu2_data = IMU_DATA(np.array(accel[1]),imu2_ref)   # imu2 gravity vector will have to be updated because main stepper rotates
+
+def getMainStepperAngle():
+    # Project the gravity vector into the plane made by ref_vectors[0] and ref_vectors[1]
+    imu1_gravity=projection_on_plane(imu1_data.refVectors[0], imu1_data.refVectors[1], imu1_data.gravityVector)
+    imu1_vertical = -1 * imu1_gravity
+    vertical_angle = getAngleFromCoordinate(imu1_vertical[0], imu1_vertical[1])
+    # Get the hole which is closest to the vertical angle
+    imu1_hole_angle = closestAngle(holeList, vertical_angle)
+    # Get the angle between the vertical angle and the hole
+    return getAngleBetween(imu1_hole_angle, vertical_angle)
+
+def getMicroStepperAngle(traveledAngle):
+    # Update imu2's gravity vector
+    imu2_data.gravityVector = np.array(getAcceleration()[1])
+    #Fith invert the imu2 gravity vector
+    imu2_gravity = projection_on_plane(imu2_data.refVectors[0], imu2_data.refVectors[1], imu2_data.gravityVector)
+    imu2_vertical = -1 * imu2_gravity
+    imu2_vertical_angle = getAngleFromCoordinate(imu2_vertical[0], imu2_vertical[1])
+    #Sixth add the vertical deviation to the imu2 vertical angle
+    imu2_hole_angle = imu2_vertical_angle + traveledAngle
+    #Get the angle between the align vector of imu2 and the hole
+    camera_2d = projection_on_plane(imu2_data.refVectors[0], imu2_data.refVectors[1], camera_vector)
+    camera_angle = getAngleFromCoordinate(camera_2d[0], camera_2d[1])
+    travel_angle = getAngleBetween(imu2_hole_angle, camera_angle)
+    #Return the angle to travel
+    return travel_angle
+
+# Checks if the main stepper rotated by correct amount
+def checkMainStepperRotation(traveledAngle):
+    beforeRot = imu2_data.gravityVector;
+    afterRot = np.array(getAcceleration()[1])
     
+
 def GetTravelAngle(imu1_data, imu2_data, holeList, camera_vector):
     #Project the gravity vector into the plane made by ref_vectors[0] and ref_vectors[1]
     imu1_gravity=projection_on_plane(imu1_data.refVectors[0], imu1_data.refVectors[1], imu1_data.gravityVector)
@@ -67,23 +115,6 @@ def open_json_file(file_path):
     with open(file_path, 'r') as json_file:
         data = json.load(json_file)
     return data
-
-def computeStepperTravelAngle(cameraAngle, holeList, imu1_accel):
-    imu1_ref = [np.array([1,0,0]), np.array([0,0,1])]
-    imu1_data = IMU_DATA(imu1_accel,imu1_ref)
-    imu1_gravity=projection_on_plane(imu1_data.refVectors[0], imu1_data.refVectors[1], imu1_data.gravityVector)
-    imu1_vertical = -1 * imu1_gravity
-    vertical_angle = getAngleFromCoordinate(imu1_vertical[0], imu1_vertical[1])
-    imu1_hole_angle = closestAngle(holeList, vertical_angle)
-    return getAngleBetween(imu1_hole_angle, vertical_angle)
-
-def computeCameraTiltAngle(holes):
-    imu1_ref = [np.array([1,0,0]), np.array([0,0,1])]
-    imu2_ref = [np.array([0,0,-1]), np.array([0,1,0])]
-    imu1_data = IMU_DATA(np.array([-1,0,-1]),imu1_ref)
-    imu2_data = IMU_DATA(np.array([0,-1,0]),imu2_ref)
-    return GetTravelAngle(imu1_data, imu2_data, holes, np.array([0,-1,0]))
-
 
 if __name__=="__main__":
     data=open_json_file("imu_data.json")
