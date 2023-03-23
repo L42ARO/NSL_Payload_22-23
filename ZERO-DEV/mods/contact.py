0b83462c71b3523b45c3df0d50313423aa4d2135
rio-okada <rokada@usf.edu>
2023-03-23 17:56:01 -0400

    Comment explaining each command

--- ZERO-DEV/mods/contact.py
+++ ZERO-DEV/mods/contact.py
@@ -1,3 +1,14 @@
+'''
+    A1—Turn camera 60º to the right
+    B2—Turn camera 60º to the left
+    C3—Take picture
+    D4—Change camera mode from color to grayscale
+    E5—Change camera mode back from grayscale to color
+    F6—Rotate image 180º (upside down).
+    G7—Special effects filter (Apply any filter or image distortion you want and
+    state what filter or distortion was used).
+'''
+
 # Default sequence will be A1, C3, B2, B2, D4, C3, A1, C3, A1, A1, A1, F6, C3
 default_seq = ["A1", "C3", "B2", "B2", "D4", "C3", "A1", "C3", "A1", "A1", "A1", "F6", "C3"]
 def GetRAFCOSequence():
