401454d426d83c0f8374da0c858be28cf1334a16
L42ARO <alvaro512@usf.edu>
2023-04-03 19:30:02 -0400

    test test

--- TestingDev/DRATest/test.py
+++ TestingDev/DRATest/test.py
@@ -1,21 +1,12 @@
 import serial
 
 # Open serial port
-ser = serial.Serial('/dev/ttyS0', 9600, timeout=1)
-
-# Send command to set frequency to 144.390 MHz
-ser.write(b'AT+DMOSETGROUP=0,144390000,0,0\r\n')
-
-# Read response from module
-response = ser.readline().decode('utf-8').strip()
-print(response)
-
-# Read response from module
-response = ser.readline().decode('utf-8').strip()
-print(response)
+ser = serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
 
 # Send command to establish connection
-ser.write(b'AT+DMOCONNECT\r\n')
+command = "AT+DMOCONNECT \r\n"
+ser.write(command.encode())
+
 
 # Read response from module
 response = ser.readline().decode('utf-8').strip()
