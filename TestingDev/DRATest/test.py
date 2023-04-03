a1cb9f14440f15474609908f02f2243c1024e305
eroset3 <evelyntrivette@yahoo.com>
2023-04-03 18:55:47 -0400

    Create test.py
    Test to communicate w/ DRA818V

---
+++ TestingDev/DRATest/test.py
@@ -0,0 +1,25 @@
+import serial
+
+# Open serial port
+ser = serial.Serial('COM1', 9600, timeout=1)
+
+# Send command to set frequency to 144.390 MHz
+ser.write(b'AT+DMOSETGROUP=0,144390000,0,0\r\n')
+
+# Read response from module
+response = ser.readline().decode('utf-8').strip()
+print(response)
+
+# Read response from module
+response = ser.readline().decode('utf-8').strip()
+print(response)
+
+# Send command to establish connection
+ser.write(b'AT+DMOCONNECT\r\n')
+
+# Read response from module
+response = ser.readline().decode('utf-8').strip()
+print(response)
+
+# Close serial port
+ser.close()
